d04654dfe0cc1320ccfa4654e2fac7864f313066
Oliver Graml <o.graml@gmx.de>
2022-01-02 23:01:56 +0100

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from 'react';
+import {useEffect, useState} from 'react';
+import styled from 'styled-components';
+import {Switch, Route} from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>website-oliver-graml</p>
       </header>
     </div>
   );
